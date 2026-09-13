082dea87351678b5d57ecb35d0a7bf33c23459d4
DUFA14 <Glebggg10@gmail.com>
2026-09-13 07:43:20 +0300

    desktop: checkpoint DayTitles

--- src/Vograph.Desktop/Domain/DayTitles.cs
+++ src/Vograph.Desktop/Domain/DayTitles.cs
@@ -15,9 +15,8 @@ public static class DayTitles
 
     public static string Subtitle(DateTime date, bool isOdd, int weekNumber, int lessonCount, Loc loc)
     {
-        var en = loc.Language == "en";
-        var culture = CultureInfo.GetCultureInfo(en ? "en-US" : "ru-RU");
-        var dateText = date.ToString(en ? "MMMM d" : "d MMMM", culture);
+        var culture = CultureInfo.GetCultureInfo("ru-RU");
+        var dateText = date.ToString("d MMMM", culture);
         var count = lessonCount == 0 ? loc.T("noLessonsShort") : loc.Plural(lessonCount, "lessons1", "lessons2", "lessons5");
         return string.Join(" · ",
             $"{loc.I18n.FormatDayFull(date)}, {dateText}",
@@ -40,5 +39,5 @@ public static class DayTitles
         var other => other
     };
 
-    public static string ShortDate(DateTime d, Loc loc) => d.ToString(loc.Language == "en" ? "MM-dd" : "dd.MM");
+    public static string ShortDate(DateTime d, Loc loc) => d.ToString("dd.MM");
 }
